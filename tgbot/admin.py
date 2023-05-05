1d2563a5cbf264c7b987dd8604783c86d3bb9534
GeorgyBatishchev <gojkljkl@gmail.com>
2023-05-05 16:09:28 +0700

    improve admin panel

--- tgbot/admin.py
+++ tgbot/admin.py
@@ -1,28 +1,64 @@
 from django.contrib import admin
+from django.db import models
+from django.db.models import Q
+
 from .models import Profile, Image, ImageScore
 
 
 @admin.register(Profile)
 class ProfileAdmin(admin.ModelAdmin):
-    list_display = ('tg_id', 'name', 'last_activity', 'last_bot_message')
+    list_display = ('tg_id', 'name', 'images_uploaded', 'score_count', 'last_activity', 'last_bot_message')
     search_fields = ('tg_id', 'name')
-    # list_editable = ('name', 'last_activity', 'last_bot_message')
+
+    def get_queryset(self, request):
+        qs = super(ProfileAdmin, self)\
+            .get_queryset(request)\
+            .annotate(image__count=models.Count('image', distinct=True))\
+            .annotate(
+                image_score__count=models.Count('image_score', distinct=True) - models.Count('image', distinct=True)
+            )
+        return qs
+
+    @admin.display(ordering="image__count", description='Загружено изображений')
+    def images_uploaded(self, obj):
+        return obj.image__count
+
+    @admin.display(ordering="image_score__count", description='Поставлено оценок')
+    def score_count(self, obj):
+        return obj.image_score__count
 
 
 @admin.register(Image)
 class ImageAdmin(admin.ModelAdmin):
     list_display = (
         'profile',
-        'file_id',
         'file_unique_id',
-        'phash',
         'datetime',
+        'likes_count',
+        'dislikes_count',
+        'file_id',
+        'phash',
     )
-    search_fields = ('profile', 'file_unique_id', 'phash')
+    search_fields = ('profile__name', 'profile__tg_id', 'file_unique_id', 'phash')
+
+    def get_queryset(self, request):
+        qs = super(ImageAdmin, self)\
+            .get_queryset(request)\
+            .annotate(likes__count=models.Count('image_score', distinct=True, filter=Q(image_score__score__gte=1)))\
+            .annotate(dislikes__count=models.Count('image_score', distinct=True, filter=Q(image_score__score__lte=0)))
+        return qs
+
+    @admin.display(ordering="likes__count", description='likes')
+    def likes_count(self, obj):
+        return obj.likes__count
+
+    @admin.display(ordering="dislikes__count", description='dislikes')
+    def dislikes_count(self, obj):
+        return obj.dislikes__count
 
 
 @admin.register(ImageScore)
 class ImageScoreAdmin(admin.ModelAdmin):
     list_display = ('profile', 'image', 'score', 'datetime')
     list_filter = ('score', )
-    search_fields = ('profile', 'image')
+    search_fields = ('profile__name', 'profile__tg_id', 'image__file_unique_id', 'image__phash')
