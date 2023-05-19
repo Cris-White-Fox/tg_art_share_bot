185be1a823b2462f0a59815a965654b90cbf7cc9
GeorgyBatishchev <gojkljkl@gmail.com>
2023-05-20 00:40:39 +0700

    add admin filters

--- tgbot/admin.py
+++ tgbot/admin.py
@@ -4,6 +4,47 @@ from django.db.models import Q
 from .models import Profile, Image, ImageScore, Report, ImageBlock
 
 
+class ActivityFilter(admin.SimpleListFilter):
+    title = 'Активность пользователей'
+    parameter_name = 'activity'
+
+    def lookups(self, request, model_admin):
+        return (
+            ('img_1', '1+ изображение'),
+            ('img_100', '100+ изображений'),
+            ('img_1000', '1000+ изображений'),
+            ('scr_1', '1+ оценка'),
+            ('scr_100', '100+ оценок'),
+            ('scr_1000', '1000+ оценок'),
+        )
+
+    def queryset(self, request, queryset):
+        if self.value() == 'img_1':
+            return queryset.annotate(
+                image_count=models.Count('image', distinct=True)
+            ).filter(image_count__gte=1).order_by('-image_count')
+        elif self.value() == 'img_100':
+            return queryset.annotate(
+                image_count=models.Count('image', distinct=True)
+            ).filter(image_count__gte=100).order_by('-image_count')
+        elif self.value() == 'img_1000':
+            return queryset.annotate(
+                image_count=models.Count('image', distinct=True)
+            ).filter(image_count__gte=1000).order_by('-image_count')
+        elif self.value() == 'scr_1':
+            return queryset.annotate(
+                image_score_count=models.Count('image_score', distinct=True)
+            ).filter(image_score_count__gte=1).order_by('-image_score_count')
+        elif self.value() == 'scr_100':
+            return queryset.annotate(
+                image_score_count=models.Count('image_score', distinct=True)
+            ).filter(image_score_count__gte=100).order_by('-image_score_count')
+        elif self.value() == 'scr_1000':
+            return queryset.annotate(
+                image_score_count=models.Count('image_score', distinct=True)
+            ).filter(image_score_count__gte=1000).order_by('-image_score_count')
+
+
 @admin.register(Profile)
 class ProfileAdmin(admin.ModelAdmin):
     list_display = (
@@ -14,17 +55,9 @@ class ProfileAdmin(admin.ModelAdmin):
         'last_activity',
         'last_bot_message'
     )
+    list_filter = (ActivityFilter,)
     search_fields = ('tg_id', 'name')
     list_per_page = 25
-
-    # def get_queryset(self, request):
-    #     qs = super(ProfileAdmin, self)\
-    #         .get_queryset(request)\
-    #         .annotate(
-    #             image__count=models.Count('image', distinct=True),
-    #             scores_from=models.Count('image_score', distinct=True) - models.F('image__count'),
-    #         )
-    #     return qs
 
     @admin.display(description='Загружено изображений')
     def images_uploaded(self, obj):
