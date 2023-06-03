5c16c505772acad5a4325e94e57c2d2879c0bd4a
GeorgyBatishchev <gojkljkl@gmail.com>
2023-06-03 19:40:51 +0700

    update photo upload

--- tgbot/views.py
+++ tgbot/views.py
@@ -1,6 +1,7 @@
 import concurrent
 
 import logging
+import time
 import traceback
 
 import telebot
@@ -9,6 +10,7 @@ from django.views.decorators.csrf import csrf_exempt
 from tgbot.tg_logic import bot, timers_view
 
 
+LAST_PHOTO = time.time()
 UPDATE_QUEUE = []
 UPDATE_IDS = []
 
@@ -31,26 +33,27 @@ def telegram_handle(request):
     if request.method == 'POST':
         global UPDATE_QUEUE
         global UPDATE_IDS
+        global LAST_PHOTO
         data = request.body.decode("utf-8")
         update = telebot.types.Update.de_json(data)
         if update.update_id in UPDATE_IDS:
             return JsonResponse({"ok": "POST processed"})
-        UPDATE_IDS = [update.update_id] + UPDATE_IDS[:1000]
 
-        if update.message and update.message.photo:
+        if update.message and update.message.photo and time.time() - LAST_PHOTO < 1:
             UPDATE_QUEUE.append(update)
+            LAST_PHOTO = time.time()
 
         else:
             process_new_update(update)
             if UPDATE_QUEUE:
-                part_to_update, UPDATE_QUEUE = UPDATE_QUEUE[:10], UPDATE_QUEUE[10:]
-                threaded_process_new_updates(part_to_update)
+                threaded_process_new_updates(UPDATE_QUEUE)
+                UPDATE_QUEUE = []
 
-        if len(UPDATE_QUEUE) > 30:
-            part_to_update, UPDATE_QUEUE = UPDATE_QUEUE[:10], UPDATE_QUEUE[10:]
-            threaded_process_new_updates(part_to_update)
+        if len(UPDATE_QUEUE) > 15:
+            threaded_process_new_updates(UPDATE_QUEUE)
+            UPDATE_QUEUE = []
 
-
+        UPDATE_IDS = [update.update_id] + UPDATE_IDS[:1000]
         return JsonResponse({"ok": "POST processed"})
     else:
         return JsonResponse({"ok": "GET processed", "TIMERS": timers_view()})
